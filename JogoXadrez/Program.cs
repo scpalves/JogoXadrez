41df4eddb925ce957e9a35cf095024929512570b
scpalves <scpalves@gmail.com>
2018-05-27 11:48:48 +0100

    Criada Posicao

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TabuleiroData;
 
 namespace JogoXadrez
 {
@@ -10,6 +11,13 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição " + P);
+
+
         }
     }
 }
---
+++ JogoXadrez/TabuleiroData/Posicao.cs
@@ -0,0 +1,25 @@
+﻿
+namespace TabuleiroData
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        
+        }
+
+        public override string ToString()
+        {
+            return "Linha: " + linha
+            + ", Coluna: "
+            + coluna;
+           
+
+        }
+    }
+}
