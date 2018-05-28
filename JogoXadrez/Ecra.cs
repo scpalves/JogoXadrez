dbe50342ed26f149a22ec08ab859ce31bc204b9c
scpalves <scpalves@gmail.com>
2018-05-28 20:28:34 +0100

    Dar cor as pecas

--- JogoXadrez/Ecra.cs
+++ JogoXadrez/Ecra.cs
@@ -11,15 +11,18 @@ namespace JogoXadrez
 
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
+                    
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
 
                 }
@@ -27,6 +30,25 @@ namespace JogoXadrez
                 Console.WriteLine();
 
             }
+
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
+            }
+
         }
     }
 
--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -13,28 +13,29 @@ namespace JogoXadrez
         static void Main(string[] args)
             {
 
-                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+                //PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-                Console.WriteLine(pos);
+                //Console.WriteLine(pos);
 
-                Console.WriteLine(pos.toPosicao());
+                //Console.WriteLine(pos.toPosicao());
 
-            //    try 
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+                try
+                {
+                    Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 2));
 
-            //    Ecra.imprimirTabuleiro(tab);
-            //}
-            //catch (Exception e)
-            //{
+                    Ecra.imprimirTabuleiro(tab);
+                }
+                catch (Exception e)
+                {
 
-            //    Console.WriteLine(e.Message);
-            //}
+                    Console.WriteLine(e.Message);
+                }
 
 
                 Console.ReadKey();
